94f87fbeac45f0db901f48fc1a2bf00c84725119
dennis <chuprakovden@yandex.ru>
2021-05-23 09:52:19 +0300

    Initial commit

---
+++ src/DVB T2 Receiver.cpp
@@ -0,0 +1,48 @@
+﻿// DVB T2 Receiver.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <complex>
+
+
+class P1_detection
+{
+    typedef std::complex<float> IQf;
+public:
+
+    void init()
+    {
+        phase = 1;
+        theta = std::polar(1.f, 2* PI * f_SH / f_d);
+        delay_line_Ta.resize(Ta);
+        delay_line_Tb.resize(Tb);
+        delay_line_Tc.resize(Tc);
+    }
+
+    void autocorr( std::vector<IQf> &piece_of_signal)
+    {
+        for (int i = 0; i < piece_of_signal.size(); i++)
+        {
+            phase *= theta;
+            phase /= abs(phase);
+
+            IQf down_shifter_sample = phase * piece_of_signal[i];
+
+
+        }
+    }
+private:
+    int Ta, Tb, Tc;
+    const float PI = 3.1415926;
+    float f_SH,f_d;
+    IQf theta, phase;
+    std::vector<IQf> delay_line_Tb, delay_line_Tc, delay_line_Ta;
+};
+
+int main()
+{
+    
+}
+
+
